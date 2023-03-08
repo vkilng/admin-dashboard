9e2062af2b1d4c7e4b2fdfd9f705808c538d1416
unknown <vinithkaradi747@gmail.com>
2023-03-08 10:32:06 +0530

    css revamped for mobile

---
+++ script.js
@@ -0,0 +1,17 @@
+const menuButton = document.querySelector('.mobile-menu-icon');
+const sidebar = document.querySelector('.sidebar');
+
+menuButton.addEventListener('click', () => {
+    sidebar.classList.toggle('show');
+    console.log('event listener 1 triggered');
+})
+
+document.addEventListener('click', (e) => {
+    if (sidebar.classList.contains('show')
+        && !e.target.classList.contains('sidebar')
+        && e.target !== menuButton) {
+        sidebar.classList.remove('show');
+        console.log(e.target.className);
+    }
+    console.log('event listener 2 triggered');
+})
